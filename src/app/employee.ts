3923bab2b03bdb57e6c29aa3d1b9feab18d2f1e0
amogh <camogh@gmail.com>
2018-06-03 12:41:21 +0530

    With Angular code

---
+++ src/app/employee.ts
@@ -0,0 +1,21 @@
+/* Employee.ts represents the model class which will have
+Have all variable from database fields.
+*/
+export class EmployeeRegistration {
+    empId: number;
+    empName:  string;
+    empEmail:  string;
+    empMobile:  string;
+    empGender:  string;
+    empAddress:  string;;
+    empCity:  string;
+    empNationality:  string;
+    empDob:  string;
+    empRole:  string;
+    empPassword:  string;
+    empStatus:  string;
+    IBUName:  string;
+    supervisorId:  number;
+    supervisorName:  string;
+    supervisorContact:  string;
+}
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+addData(empId: number,  empName:  string,  empEmail:  string, empMobile:  string,  empGender:  string,  empAddress:  string,  empCity:  string,  empNationality:  string,  empDob:  string,  empRole:  string,  empPassword:  string,  empStatus:  string,  IBUName:  string,  supervisorId:  number,  supervisorName: string,  supervisorContact: string)
+{
+
+}
+}
